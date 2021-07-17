8ebc771a3a666a6ce770dd5cc69fd71b0be5b62f
Ashraf Qasem <77658275+ashrafqasem@users.noreply.github.com>
2021-07-17 13:08:41 -0400

    00 Big_O

---
+++ _00_Big_O/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Big_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nemo = new string[1] { "nemo" };
+            findNemo(nemo);
+
+            Console.ReadKey();
+        }
+
+        static void findNemo(string[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == "nemo")
+                {
+                    Console.WriteLine("Found Nemo!");
+                }
+            }
+        } 
+
+    }
+}
