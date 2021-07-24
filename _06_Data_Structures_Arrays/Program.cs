aa5edbce5480120b89e76976cfd24c6ac6bcc4ab
Ashraf Qasem <77658275+ashrafqasem@users.noreply.github.com>
2021-07-24 15:06:45 -0400

    TwoSum2

--- _06_Data_Structures_Arrays/Program.cs
+++ _06_Data_Structures_Arrays/Program.cs
@@ -22,14 +22,33 @@ namespace _06_Data_Structures_Arrays
         /* 070 mergeSortedArrays-exercise-Repl https://repl.it/@aneagoie/MergeSortedArrays-exercise */
         /* 071 Solution-Code https://replit.com/@aneagoie/MergeSortedArrays */
 
+        /* leetcode */
+        /* 1. Two Sum: https://leetcode.com/problems/two-sum/description/ */
+        /* 53. Maximum Subarray: https://leetcode.com/problems/maximum-subarray/description/ */
+        /* 283. Move Zeroes: https://leetcode.com/problems/move-zeroes/description/ */
+        /* 217. Contains Duplicate: https://leetcode.com/problems/contains-duplicate/description/ */
+        /* 189. Rotate Array: https://leetcode.com/problems/rotate-array/description/ */
+
+        /* regex: https://regex101.com/ */
+
+        /* coderbyte */
+        /* Longest Word: https://coderbyte.com/information/Longest%20Word */
+
+
         static void Main(string[] args)
         {
             //ImplementArray();
             //ReverseString();
             //ReverseString2();
             //ReverseString3();
-            MergeSortedArrays(new int[] { 0, 3, 4, 31 }, new int[] { 4, 6, 30 });
+            //MergeSortedArrays(new int[] { 0, 3, 4, 31 }, new int[] { 4, 6, 30 });
             //MergeSortedArrays2(new int[] { 0, 3, 4, 31, 37, 39, 43, 48 }, new int[] { 4, 6, 30, 32, 33, 34, 35, 38, 40, 41, 44, 46, 47 });
+            //TwoSum(new int[] { 2,7,11,15}, 9);
+            //TwoSum(new int[] { 3, 2, 4 }, 6);
+            //TwoSum(new int[] { 3, 3 }, 6);
+            TwoSum2(new int[] { 2, 7, 11, 15 }, 9);
+            TwoSum2(new int[] { 3, 2, 4 }, 6);
+            TwoSum2(new int[] { 3, 3 }, 6);
 
             Console.ReadKey();
         }
@@ -225,6 +244,181 @@ namespace _06_Data_Structures_Arrays
 
             Console.WriteLine(string.Join(",", mergedArray));
             return mergedArray;
+        }
+        #endregion
+
+        #region [1. Two Sum]
+        #region [?]
+        /*
+            Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+            You may assume that each input would have exactly one solution, and you may not use the same element twice.
+            You can return the answer in any order.
+
+            Example 1:
+                Input: nums = [2,7,11,15], target = 9
+                Output: [0,1]
+                Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+            Example 2:
+                Input: nums = [3,2,4], target = 6
+                Output: [1,2]
+
+            Example 3:
+                Input: nums = [3,3], target = 6
+                Output: [0,1]
+
+            Constraints:
+                2 <= nums.length <= 104
+                -109 <= nums[i] <= 109
+                -109 <= target <= 109
+                Only one valid answer exists.
+
+            Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+        */
+        #endregion
+        public static int[] TwoSum(int[] nums, int target) // No Good! Nested Loops! Time complexity O(n^2)
+        {
+            int[] output = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for(int j = 1 + i; j < nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        output[0] = i;
+                        output[1] = j;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(",", output));
+            return output;
+        }
+        #endregion
+
+        #region [1. Two Sum 2]
+        /*
+            Complexity:
+                Time: O(N) where N is the number of intergers in the input array.
+                We basically just need to traverse the input array one time. Dictioary.Add() and Dictioanry.ContainsKey() are both O(1) operation.
+
+                Space: O(N) where N is the number of intergers in the input array.
+                Dictonary stores at most n number of key-value pairs.
+
+            Note:
+                There are two ways to initialize an empty array in C#:
+                    new int[size];
+                    new int[]{};
+        */
+
+
+        public static int[] TwoSum2(int[] nums, int target) // Good! Time complexity O(n)
+        {
+            if (nums == null || nums.Length < 2)
+            {
+                return new int[2];
+            }
+
+            int[] output = new int[2]; // Ok int[] output = new int[] { };
+            Dictionary<int, int> complementDictionary = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                if (complementDictionary.ContainsKey(target - nums[i])) // checks if compliment is in complementDictionary
+                {
+                    //return new int[] { complementDictionary[target - nums[i]], i }; // ok
+                    output[0] = complementDictionary[target - nums[i]]; // Or complementDictionary.Where(x => x.Key == nums[i]).FirstOrDefault().Value; 
+                    output[1] = i;
+                    break;
+                }
+                else if (!complementDictionary.ContainsKey(nums[i])) // handles duplicates in complementDictionary
+                //else
+                {
+                    complementDictionary.Add(nums[i], i);
+                }               
+            }
+
+            //Console.WriteLine(string.Join(",", output));
+            return output;
+        }
+        #endregion
+
+        #region []
+        /*
+
+        */
+        static int[] xx() // 
+            {
+
+                return null;
+            }
+            #endregion
+
+        #region []
+        /*
+
+        */
+        static int[] xxx() // 
+        {
+
+            return null;
+        }
+        #endregion
+
+        #region []
+        /*
+
+        */
+        static int[] xxxx() // 
+        {
+
+            return null;
+        }
+        #endregion
+
+        #region []
+        /*
+
+        */
+        static int[] xxxxx() // 
+        {
+
+            return null;
+        }
+        #endregion
+
+        #region []
+        /*
+
+        */
+        static int[] xxxxxx() // 
+        {
+
+            return null;
+        }
+        #endregion
+
+        #region []
+        /*
+
+        */
+        static int[] xxxxxxx() // 
+        {
+
+            return null;
+        }
+        #endregion
+
+        #region []
+        /*
+
+        */
+        static int[] xxxxxxxx() // 
+        {
+
+            return null;
         }
         #endregion
     }
